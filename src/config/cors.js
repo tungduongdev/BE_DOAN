e65920a83a3a70b0ab83157763426692de8e50c6
tungduong <tungduong2550@gmail.com>
2025-05-30 20:37:41 +0700

    fix

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,33 +4,12 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 
-import { WHITELIST_DOMAINS } from '../utils/constants.js'
 import { env } from './environment.js'
-import { StatusCodes } from 'http-status-codes'
-import ApiError from '../utils/ApiError.js'
 
-// Cấu hình CORS Option trong dự án thực tế (Video số 62 trong chuỗi MERN Stack Pro)
+// Cấu hình CORS Option để cho phép tất cả domain truy cập API
 export const corsOptions = {
-  origin: function (origin, callback) {
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    // Update mới: Ở video số 75 trong chuỗi MERN Stack PRO khi chúng ta deploy dự án lên một Server Production thì sẽ sửa lại đoạn này thêm một chút nữa để phù hợp với từng môi trường production hoặc dev nhé. Học với mình thì các bạn cứ yên tâm về sự chỉn chu chuẩn chỉnh nhé :D
-    if (!origin && env.BUILD_MODE === 'dev') {
-      return callback(null, true)
-    }
-
-    // Kiểm tra xem origin có phải là domain được chấp nhận hay không
-    if (WHITELIST_DOMAINS.includes(origin)) {
-      return callback(null, true)
-    }
-
-    // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
-    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
-  },
-
-  // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  optionsSuccessStatus: 200,
-
-  // CORS sẽ cho phép nhận cookies từ request, (Nhá hàng :D | Ở khóa MERN Stack Advance nâng cao học trực tiếp mình sẽ hướng dẫn các bạn đính kèm jwt access token và refresh token vào httpOnly Cookies)
-  credentials: true
+  origin: true, // Cho phép tất cả domain
+  credentials: true, // Cho phép gửi cookie qua CORS
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }
